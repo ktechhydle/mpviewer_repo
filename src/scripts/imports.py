8e3290d618b9ccaf0720c46362194a26d63e3d1d
kelle <kellerhydle@gmail.com>
2024-06-22 22:10:15 -0600

    #2 major changes
    -added gui
    -open file works
    -fixed major issues
    -removed directories

--- src/scripts/imports.py
+++ src/scripts/imports.py
@@ -17,19 +17,3 @@ from PyQt5.Qt import *
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
-from scipy.interpolate import splprep, splev
-from scipy.signal import savgol_filter
-from skimage.measure import *
-from shapely.geometry import Polygon
-from shapely.geometry.polygon import orient
-
-# Import custom packages
-from src.framework.undo_commands import *
-from src.framework.custom_classes import *
-from src.framework.custom_classes import *
-from src.scripts.app_screens import *
-from src.scripts.styles import *
-from src.scripts.app_internal import *
-from src.gui.custom_widgets import *
-from src.gui.libraries import *
-from src.gui.custom_dialogs import *
